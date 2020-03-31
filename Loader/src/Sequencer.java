c8ae2a69aa3184815c2438c9c33496abb75695b4
Nikita <43483640+Si-ja@users.noreply.github.com>
2020-03-31 17:57:17 +0200

    Update Sequencer.java

--- Loader/src/Sequencer.java
+++ Loader/src/Sequencer.java
@@ -3,7 +3,7 @@
 /**
  * Sequencer allows to calculate numbers and their positions depending on what is processed.
  * It can tell the number in a Fibonaci sequence, or in Prime Numbers.
- * @author Nikita
+ * @author Si-ja
  *
  */
 
@@ -15,11 +15,13 @@ public class Sequencer {
 	 * be done through a recursion...buuuuuuuuuuuuuuut. We have a different goal. And that is to implement the visual loader.
 	 */
 	
+	//Keep track of few variables that could be re-used for future methods
 	private final Integer start = 0;
 	private final Integer storage1 = 0;
 	private final Integer storage2 = 0;
 	private final Integer answer = 0;
 	
+	//Load the class that essentially allows for calculating the progression in our sequences
 	private LoaderHelper loader = new LoaderHelper();
 	
 	//=================================================================================================================
@@ -30,15 +32,17 @@ public class Sequencer {
 	 */
 	public Integer calculateFibonacci(int end) 
 	{		
+		//Load all the needed variables
 		Integer start = this.start;
 		Integer storage1 = this.storage1;
 		Integer storage2 = this.storage2;
 		Integer answer = this.answer;
 		
-		storage1 = 0;
-		storage2 = 1;
-		answer = null;
+		storage1 = 0;  //Represents the F(n-2) value in the Fibonacci sequence
+		storage2 = 1;  //Represents the F(n-1) value in the Fibonacci sequence
+		answer = null; //Represents the F(n) value in the Fibonacci sequence
 		
+		//Make few conditions, as we cannot start from the value in sequence 1 or 2, with a repeating eas
 		if (end == 0) 
 		{
 			answer = storage1;
@@ -49,15 +53,17 @@ public class Sequencer {
 			return answer;
 		}
 		
+		//If the value passed by the user is 2 or higher, we will alway start here.
 		//Start the execution process of where the number needs to accumulate and calculated
 		for (int i = start; i <= end - 1; i++) 
 		{
 			answer = storage1 + storage2;
 			storage1 = storage2;
 			storage2 = answer;
-			loader.EvaluatorApplicable(i, end - 1);
+			loader.EvaluatorApplicable(i, end - 1); //Loader evaluates how far are we in our calculations process
+			                                        //And prints (updates) the progression bar
 		}
-		return answer;
+		return answer; //Give the users the answer
 	}
 
 }
